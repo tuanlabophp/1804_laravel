7bec4cc9d55dd52e4d5bbcde79a15664eb440858
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2018-10-19 21:29:31 +0700

    Buoi 38 - Migration + Seeder

---
+++ laravel/database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // if (DB::table('categories')->get()->count() == 0) {
+        	DB::insert([
+        		'id' => 1,
+        		'name' => 'First Category'
+        	]);
+        // } else {
+        // 	echo "Table Categories is not empty!";
+        // }
+    }
+}
